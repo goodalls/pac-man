eb5019b49944479dd21516cae7b02b24f69ec916
Jeff Goodall <goodalls@live.com>
2022-12-30 16:11:42 -0700

    adding keeping score function for dots

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const scoreDisplay = document.getElementById("score");
   const width = 28; //28*28 = 784 squares
   const squares = [];
+  let score = 0
   const layout = [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
@@ -65,47 +66,47 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   createBoard();
 
-  let pacmanCurrnetIndex = 490;
-  squares[pacmanCurrnetIndex].classList.add("pac-man");
+  let pacmanCurrentIndex = 490;
+  squares[pacmanCurrentIndex].classList.add("pac-man");
 
   //move Pac-Man
   function movePacman(e) {
-    squares[pacmanCurrnetIndex].classList.remove("pac-man");
+    squares[pacmanCurrentIndex].classList.remove("pac-man");
     switch (e.keyCode) {
         case 37: //left arrow key
-            if(pacmanCurrnetIndex % width !== 0 && !squares[pacmanCurrnetIndex -1].classList.contains('wall') && !squares[pacmanCurrnetIndex -1].classList.contains('lair'))
-            pacmanCurrnetIndex -=1
+            if(pacmanCurrentIndex % width !== 0 && !squares[pacmanCurrentIndex -1].classList.contains('wall') && !squares[pacmanCurrentIndex -1].classList.contains('lair'))
+            pacmanCurrentIndex -=1
           
             //check for left exit
-            if (pacmanCurrnetIndex -1 === 363) {
-                squares[pacmanCurrnetIndex].classList.add('pac-man')
-                pacmanCurrnetIndex = 391
+            if (pacmanCurrentIndex -1 === 363) {
+                squares[pacmanCurrentIndex].classList.add('pac-man')
+                pacmanCurrentIndex = 391
                 squares[364].classList.remove('pac-man')
             }
             break;
         case 38: //up arrow key
-            if (pacmanCurrnetIndex - width >= 0 && !squares[pacmanCurrnetIndex -width].classList.contains('wall') && !squares[pacmanCurrnetIndex -width].classList.contains('lair')) 
-            pacmanCurrnetIndex -=width
+            if (pacmanCurrentIndex - width >= 0 && !squares[pacmanCurrentIndex -width].classList.contains('wall') && !squares[pacmanCurrentIndex -width].classList.contains('lair')) 
+            pacmanCurrentIndex -=width
             break;
         case 39: //right arrow key
-            if (pacmanCurrnetIndex % width < width -1 && !squares[pacmanCurrnetIndex +1].classList.contains('wall') && !squares[pacmanCurrnetIndex +1].classList.contains('lair')) 
-            pacmanCurrnetIndex +=1
+            if (pacmanCurrentIndex % width < width -1 && !squares[pacmanCurrentIndex +1].classList.contains('wall') && !squares[pacmanCurrentIndex +1].classList.contains('lair')) 
+            pacmanCurrentIndex +=1
 
              //check for right exit
-             if(pacmanCurrnetIndex +1 === 392) {
-                squares[pacmanCurrnetIndex].classList.add('pac-man')
-                pacmanCurrnetIndex = 364
+             if(pacmanCurrentIndex +1 === 392) {
+                squares[pacmanCurrentIndex].classList.add('pac-man')
+                pacmanCurrentIndex = 364
                 squares[391].classList.remove('pac-man')
             }
             break;
         case 40: //down arrow key
-            if (pacmanCurrnetIndex + width < width * width && !squares[pacmanCurrnetIndex +width].classList.contains('wall') && !squares[pacmanCurrnetIndex +width].classList.contains('lair')) 
-            pacmanCurrnetIndex +=width
+            if (pacmanCurrentIndex + width < width * width && !squares[pacmanCurrentIndex +width].classList.contains('wall') && !squares[pacmanCurrentIndex +width].classList.contains('lair')) 
+            pacmanCurrentIndex +=width
             break;
     }
-    squares[pacmanCurrnetIndex].classList.add('pac-man')
+    squares[pacmanCurrentIndex].classList.add('pac-man')
 
-    //dotEaten()
+    keepScore()
     //pelletEaten()
     //gameOver()
     //checkForWin()
@@ -113,4 +114,11 @@ document.addEventListener("DOMContentLoaded", () => {
   }
  
   document.addEventListener('keyup', movePacman)
+  function keepScore() {
+    if (squares[pacmanCurrentIndex].classList.contains('dot')) {
+        score++
+        scoreDisplay.innerHTML = score
+        squares[pacmanCurrentIndex].classList.remove('dot')
+    } 
+  }
 });
